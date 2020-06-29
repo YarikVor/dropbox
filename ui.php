53ea613f4b0372f43f76dea8997e184353bacd67
YarikVor <62393229+YarikVor@users.noreply.github.com>
2020-06-30 02:00:16 +0300

    Update ui.php

--- ui.php
+++ ui.php
@@ -1,4 +1,12 @@
-<?php
-$file = fopen ("somefile.txt", "r+");
-echo $file;
-?>
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <?php
+    $file = fopen ("somefile.txt", "r+");
+    echo $file;
+    ?>
+  
+  </body>
+</html>
+

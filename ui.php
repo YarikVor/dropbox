82c5345c90f0fc58ab9112170a2294b8aafe4dbb
YarikVor <62393229+YarikVor@users.noreply.github.com>
2020-06-30 10:40:31 +0300

    Update ui.php

--- ui.php
+++ ui.php
@@ -4,7 +4,7 @@
   <body>
     <?php
     $file = fopen ("somefile.txt", "w+");
-    fwrite ($file, "pig");
+    fwrite ($file, "pig".fgets($file));
     
     echo fgets ($file);
     fclose ($file);
